2f86ae9c4ddab17b8a4165689bfce46413a2d39b
Louis Smith <lcsmith@stsci.edu>
2015-05-24 18:58:41 -0400

    Working factor selectors. Working state selectors.

--- js/aecf-map.js
+++ js/aecf-map.js
@@ -1,14 +1,14 @@
-var width = 960,
-    height = 600;
+var width = 600,
+    height = 400,
+    centered;
 
-console.log(d3.select("#map-chart"));
 
 var svg = d3.select("#map-chart").append("svg")
     .attr("width", width)
     .attr("height", height);
 
 var projection = d3.geo.albersUsa()
-    .scale(900)
+    .scale(800)
     .translate([width / 2, height / 2]);
 
 var path = d3.geo.path()
@@ -17,56 +17,46 @@ var path = d3.geo.path()
 
 var color = d3.scale.quantize()
    .range(["rgb(240,230,200)","rgb(240,215,140)","rgb(240,205,80)","rgb(220,175,45)","rgb(200,155,5)"]);  
- 
-var zoom = d3.behavior.zoom()
-    .translate(projection.translate())
-    .scale(projection.scale())
-    .scaleExtent([height, 8 * height])
-    .on("zoom", zoomed);
 
 var g = svg.append("g")
-    .call(zoom);
+    // .call(zoom);
 
 g.append("rect")
     .attr("class", "background")
     .attr("width", width)
     .attr("height", height);
 
-d3.csv("/sandbox/data/sdr091b.csv", function(data) {
-    console.log(data);
-    color.domain([
-            d3.min(data, function(d) { return +d.DRPWH; }),
-            d3.max(data, function(d) { return +d.DRPWH; })
-    ]);
+d3.json("data/stateScores.json", function(error,data) {
+    
+    // console.log(data);
+    
 
 
-    d3.json("/sandbox/data/us-states.json", function(json) {
-        console.log(json);
+    d3.json("data/us-states.json", function(json) {
+        // console.log(json);
+        // console.log(data);
 
-        //Merge the ag. data and GeoJSON
-        //Loop through once for each ag. data value
-        for (var i = 0; i < data.length; i++) {
+        var opp_scores = [];
 
-            //Grab state name
-            var dataState = data[i].STATENAME;
-            //console.log(dataState);
+        $.each(data, function(key,val){
+            
+            var state = val.title,
+                value = val.opp_score;                
 
-            //Grab data value, and convert from string to float
-            var dataValue = parseFloat(data[i].DRPWH);
-            //console.log(dataValue);
+            opp_scores.push(value);
 
-            //Find the corresponding state inside the GeoJSON
+
             for (var j = 0; j < json.features.length; j++) {
-                //console.log(json.features)
+                // console.log(json.features)
 
                 var jsonState = json.features[j].properties.name;
                 //console.log(json.features[j].properties);
 
-                if (dataState == jsonState) {
+                if (state == jsonState) {
                     //console.log("match")
 
                     //Copy the data value into the JSON
-                    json.features[j].properties.value = dataValue;
+                    json.features[j].properties.value = value;
 
                     //Stop looking through the JSON
                     break;
@@ -74,9 +64,15 @@ d3.csv("/sandbox/data/sdr091b.csv", function(data) {
                 }
 
             }
-        }
 
-        for (var i = 0; i < data.length; i++) {
+        });
+        
+        // console.log(opp_scores);
+        color.domain([
+            d3.min(opp_scores),
+            d3.max(opp_scores)
+        ]); 
+        
             g.append("g")
                 .attr("id","states")
             .selectAll("path")
@@ -84,6 +80,7 @@ d3.csv("/sandbox/data/sdr091b.csv", function(data) {
                .enter()
                .append("path")
                .attr("d", path)
+               .attr("id",function(d,i){ return d.properties.name; })
                .style("fill", function(d) {
                     //Get data value
                     var value = d.properties.value;
@@ -96,28 +93,42 @@ d3.csv("/sandbox/data/sdr091b.csv", function(data) {
                             //If value is undefined…
                             return "#EEE";
                     }
-               });
-       }
-    })
+               })
+               .on("click",clicked);
+        })
 }) 
 
 function clicked(d) {
-  var centroid = path.centroid(d),
-      translate = projection.translate();
+    // console.log(d.properties.name);
+    state = d.properties.name
+      var x, y, k;
 
-  projection.translate([
-    translate[0] - centroid[0] + width / 2,
-    translate[1] - centroid[1] + height / 2
-  ]);
+      if (d && centered !== d) {
+        var centroid = path.centroid(d);
+        x = centroid[0];
+        y = centroid[1];
+        k = 4;
+        centered = d;
+      } else {
+        x = width / 2;
+        y = height / 2;
+        k = 1;
+        centered = null;
+      }
 
-  zoom.translate(projection.translate());
+      g.selectAll("path")
+          .classed("active", centered && function(d) { return d === centered; });
 
-  g.selectAll("path").transition()
-      .duration(700)
-      .attr("d", path);
+      g.transition()
+          .duration(750)
+          .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")scale(" + k + ")translate(" + -x + "," + -y + ")")
+          .style("stroke-width", 1.5 / k + "px");
+
+    smallMults(state);
 }
 
-function zoomed() {
+
+/*function zoomed() {
   projection.translate(d3.event.translate).scale(d3.event.scale);
   g.selectAll("path").attr("d", path);
-}
+}*/
--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
+var state = "United States",
+	factor = "count";
 
+$("#main-chart-container .factor-button").on("click", function(){
+
+console.log(this.value);
+	factor = this.value;
+console.log(state,factor);
+	smallMults(state,factor)
+})
