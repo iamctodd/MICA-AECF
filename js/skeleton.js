42438cca8191cb59277805cdbf94cecb56e8524f
Louis Smith <lcsmith@stsci.edu>
2015-05-25 00:47:11 -0400

    Trying to merge

--- js/skeleton.js
+++ js/skeleton.js
@@ -91,15 +91,4 @@ $(document).ready(function() {
 
   init();
 
-});
-
-var state = "United States",
-	factor = "count";
-
-$("#main-chart-container .factor-button").click(function(e){
-	e.preventDefault();	
-	factor = this.value;
-	smallMults(state,factor);
-	return false;
 })
-
